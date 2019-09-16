7c264375b3a8ee1e12619e1608e89fe2d0322a01
caiwenshu <wenshu.cai@shenmajr.com>
2019-09-16 11:25:00 +0800

    修复兼容问题

--- Source/NominalType.swift
+++ Source/NominalType.swift
@@ -31,7 +31,14 @@ extension NominalType {
             // swift class created dynamically in objc-runtime didn't have valid nominalTypeDescriptor
             return nil
         }
+        
+        #if swift(>=4.1) || (swift(>=3.3) && !swift(>=4.0))
+        return NominalTypeDescriptor(pointer: relativePointer(base: base, offset: base.pointee - base.hashValue))
+        #else
         return NominalTypeDescriptor(pointer: relativePointer(base: base, offset: base.pointee))
+        #endif
+        
+//        return NominalTypeDescriptor(pointer: relativePointer(base: base, offset: base.pointee))
     }
 
     var fieldTypes: [Any.Type]? {
